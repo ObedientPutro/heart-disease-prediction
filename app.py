0dc84d1e487844ad3c7fa4d7c030698f91c186f3
ObedientPutro <142576291+ObedientPutro@users.noreply.github.com>
2023-12-11 22:34:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
+import sklearn
 import numpy as np
 import pandas as pd
 import streamlit as st
-import sklearn
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
